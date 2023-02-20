201ac92cf7dcd336ca72c2912ff8d0b843fdee8b
Alberto-Bruno <brunoalberto97@outlook.it>
2023-02-20 20:38:04 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+include_once __DIR__ . '/models/pet_1.php';
+include_once __DIR__ . '/models/pet_2.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP E-commecre</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center mt-5">Tutto quello che ti serve per il tuo animale domestico!</h1>
+        <div class="row">
+            <div class="col-4 my-5">
+                <div class="card pb-5" style="width: 18rem;">
+                    <img src="" class="card-img-top" alt="">
+                    <div class="card-body text-center">
+                        <h2 class="card-title fs-3"></h2>
+                        <p class="card-text">Euro</p>
+                        <p class="card-text">Prodotto per </p>
+                        <p class="card-text fst-italic">Codice prodotto:</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ models/pet_2.php
@@ -0,0 +1,15 @@
+<?php
+include_once __DIR__ . '/products.php';
+
+class Cat extends Products
+{
+    public $type;
+    public $poster;
+
+    public function __construct($name, $price, $poster, $type)
+    {
+        $this->type = $type;
+        $this->poster = $poster;
+        parent::__construct($name, $price);
+    }
+}
